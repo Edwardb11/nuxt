55a9ad24c589f920d0e347f95d3bd630aa9b99ae
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2024-02-09 15:10:18 -0400

    feat: register logic

--- pages/register.vue
+++ pages/register.vue
@@ -1,12 +1,21 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
+import {createUserWithEmailAndPassword} from "firebase/auth"
 
 const email = ref('');
 const password = ref('');
+const {$auth}=useNuxtApp()
 
-const handleSubmit = () => {
-    console.log({ email: email.value, password: password.value })
+
+
+const handleSubmit =async () => {
+    try {
+     const res=   await createUserWithEmailAndPassword($auth,email.value, password.value);
+     console.log(res)
+    } catch (error) {
+        console.error(error);
+    }
 };
 </script>
 
@@ -15,6 +24,6 @@ const handleSubmit = () => {
     <form @submit.prevent="handleSubmit">
         <input label="Email" type="email" v-model="email" />
         <input label="Password" type="password" v-model="password" />
-        <input type="submit">Register</input>
+        <button type="submit">Registro</button>
     </form>
 </template>
