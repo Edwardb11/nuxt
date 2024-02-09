0780c19756a97a8ed505153bceb78bbcdfc0b48f
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2024-02-09 15:06:06 -0400

    feat: register

--- pages/register.vue
+++ pages/register.vue
@@ -1,3 +1,20 @@
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const email = ref('');
+const password = ref('');
+
+const handleSubmit = () => {
+    console.log({ email: email.value, password: password.value })
+};
+</script>
+
 <template>
     <h1>Register page</h1>
+    <form @submit.prevent="handleSubmit">
+        <input label="Email" type="email" v-model="email" />
+        <input label="Password" type="password" v-model="password" />
+        <input type="submit">Register</input>
+    </form>
 </template>
