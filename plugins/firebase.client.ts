767a59b3b2863c9c60989c3b47586a0e6b521766
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2024-02-12 10:02:46 -0400

    feat: configure .nev

--- plugins/firebase.client.ts
+++ plugins/firebase.client.ts
@@ -6,16 +6,23 @@ export default defineNuxtPlugin((nuxtApp) => {
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
+  const {
+    apiKey,
+    appId,
+    authDomain,
+    messagingSenderId,
+    projectId,
+    storageBucket,
+  } = useRuntimeConfig().public;
   // Your web app's Firebase configuration
   const firebaseConfig = {
-    apiKey: "AIzaSyCDlKcoKRngDN3SU4-eU7TOt02cbR65vdU",
-    authDomain: "nuxt-crud-eed64.firebaseapp.com",
-    projectId: "nuxt-crud-eed64",
-    storageBucket: "nuxt-crud-eed64.appspot.com",
-    messagingSenderId: "694311351307",
-    appId: "1:694311351307:web:33a1d6e5741a08ff30c30e",
+    apiKey,
+    authDomain,
+    projectId,
+    storageBucket,
+    messagingSenderId,
+    appId,
   };
-
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
