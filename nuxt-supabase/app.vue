977413d6797898676b821374510d15cc8772fcec
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2024-02-12 11:50:30 -0400

    feat: system routing and pages with configure supabase

--- nuxt-supabase/app.vue
+++ nuxt-supabase/app.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <nav>
+      <ul>
+        <li><nuxt-link to="/">Home</nuxt-link></li>
+        <li><nuxt-link to="/protected">Protected</nuxt-link></li>
+        <li><nuxt-link to="/auth">Auth</nuxt-link></li>
+      </ul>
+    </nav>
+    <NuxtPage />
   </div>
 </template>
--- nuxt-supabase/nuxt.config.ts
+++ nuxt-supabase/nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/supabase"],
+  supabase: {
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_KEY,
+    redirectOptions: {
+      login: "/auth",
+      callback: "/confirm",
+      exclude: ["/", "/protected"],
+    },
+  },
+});
