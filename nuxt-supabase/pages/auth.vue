109ad477b92773844cad1f602649044f0c9de819
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2024-02-12 14:39:41 -0400

    feat: styles

--- nuxt-supabase/pages/auth.vue
+++ nuxt-supabase/pages/auth.vue
@@ -1,78 +1,85 @@
 <template>
-    <section>
-  <h2>Sign Up</h2>
-<form @submit.prevent="signUp">
-    <div>
-        <label for="email">Email</label>
-        <input
-        type="email"
-        id="email"
-        v-model="createUser.email"
-        />
-    </div>
-    <div>
-        <label for="password">Password</label>
-        <input
-        type="password"
-        id="password"
-        v-model="createUser.password"
-        />
-    </div>
-    <button type="submit">Sign Up</button>
-</form>
-    </section>
-    <section>
-  <h2>login</h2>
-<form @submit.prevent="login">
-    <div>
-        <label for="email">Email</label>
-        <input
-        type="email"
-        id="email"
-        v-model="loginUser.email"
-        />
-    </div>
-    <div>
-        <label for="password">Password</label>
-        <input
-        type="password"
-        id="password"
-        v-model="loginUser.password"
-        />
-    </div>
-    <button type="submit">Sign Up</button>
-</form>
+    <section class="flex justify-center items-center h-screen">
+      <div class="max-w-md mx-auto bg-white p-8 shadow-lg rounded-lg flex">
+        <div class="w-1/2 mr-2 pr-4 border-r border-gray-300">
+          <h2 class="text-2xl font-bold mb-4">Sign Up</h2>
+          <form @submit.prevent="signUp">
+            <div class="mb-4">
+              <label for="email" class="block text-sm font-semibold mb-2">Email</label>
+              <input
+                type="email"
+                id="email"
+                v-model="createUser.email"
+                class="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
+              />
+            </div>
+            <div class="mb-4">
+              <label for="password" class="block text-sm font-semibold mb-2">Password</label>
+              <input
+                type="password"
+                id="password"
+                v-model="createUser.password"
+                class="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
+              />
+            </div>
+            <button type="submit" class="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded-lg">Sign Up</button>
+          </form>
+        </div>
+  
+        <div class="w-1/2 ml-2 pl-4">
+          <h2 class="text-2xl font-bold mb-4">Login</h2>
+          <form @submit.prevent="login">
+            <div class="mb-4">
+              <label for="email" class="block text-sm font-semibold mb-2">Email</label>
+              <input
+                type="email"
+                id="email"
+                v-model="loginUser.email"
+                class="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
+              />
+            </div>
+            <div class="mb-4">
+              <label for="password" class="block text-sm font-semibold mb-2">Password</label>
+              <input
+                type="password"
+                id="password"
+                v-model="loginUser.password"
+                class="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
+              />
+            </div>
+            <button type="submit" class="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded-lg">Login</button>
+          </form>
+        </div>
+      </div>
     </section>
   </template>
   
   <script lang="ts" setup>
-import { reactive } from 'vue'
-
-
-const supabase = useSupabaseClient()
-
-const createUser = reactive({
-  email: '',
-  password: '',
-})
-const loginUser = reactive({
-  email: '',
-  password: '',
-})
-const signUp = async () => {
-  const { user, session, error } = await supabase.auth.signUp({
-    email: createUser.email,
-    password: createUser.password,
+  import { reactive } from 'vue'
+    const supabase = useSupabaseClient()
+  
+  const createUser = reactive({
+    email: '',
+    password: '',
   })
-error ? alert(error): navigateTo('/confirm')
-}
-
-const login = async () => {
-  const { user, session, error } = await supabase.auth.signInWithPassword({
-    email: loginUser.email,
-    password: loginUser.password,
+  const loginUser = reactive({
+    email: '',
+    password: '',
   })
-error ? alert(error): navigateTo('/protected')
-}
-
-</script>
+  const signUp = async () => {
+    const {  error } = await supabase.auth.signUp({
+      email: createUser.email,
+      password: createUser.password,
+    })
+    error ? alert(error): navigateTo('/confirm')
+  }
+  
+  const login = async () => {
+    const { user, session, error } = await supabase.auth.signInWithPassword({
+      email: loginUser.email,
+      password: loginUser.password,
+    })
+    error ? alert(error): navigateTo('/protected')
+  }
+  </script>
+  
